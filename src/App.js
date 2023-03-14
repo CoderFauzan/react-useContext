ac03dda8654d2a91fccd29448a9bc0e10338a3c9
CoderFauzan <90621630+CoderFauzan@users.noreply.github.com>
2023-03-14 12:53:56 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import './style.css';
+
+import { useState, createContext, useContext } from 'react';
+import ReactDOM from 'react-dom/client';
+
+const UserContext = createContext();
+
+export default function App() {
+  const [user, setUser] = useState('Any name');
+
+  return (
+    <UserContext.Provider value={user}>
+      <h1>{`Hello ${user}!`}</h1>
+      <button type="button" onClick={() => setUser('Fauzan')}>
+        Change name
+      </button>
+      <Component2 />
+    </UserContext.Provider>
+  );
+}
+
+function Component2() {
+  return (
+    <>
+      <h1>Component 2</h1>
+      <Component3 />
+    </>
+  );
+}
+
+function Component3() {
+  return (
+    <>
+      <h1>Component 3</h1>
+      <Component4 />
+    </>
+  );
+}
+
+function Component4() {
+  return (
+    <>
+      <h1>Component 4</h1>
+      <Component5 />
+    </>
+  );
+}
+
+function Component5() {
+  const user = useContext(UserContext);
+
+  return (
+    <>
+      <h1>Component 5</h1>
+      <h2>{`Hello ${user} again!`}</h2>
+    </>
+  );
+}
